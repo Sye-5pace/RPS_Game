65585d125dea8180edd3b93ae7282dcf67acfd89
Sye-5pace <owusilasyp00@gmail.com>
2023-10-19 23:29:46 +0100

    :lipstick: Results refactored with UI

--- src/Gameboard.tsx
+++ src/Gameboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React,{ useState,useEffect } from "react"
 
 import  Paper  from './assets/paper-card.svg'
 import  Rock  from './assets/rock-card.svg'
@@ -31,11 +31,32 @@ const Gameboard: React.FC<GameboardProps> = ({setScore}) => {
     const { playerChoice, computerChoice, result, play, reset } = useRPSGame(setScore);
     const [ playerSelected, setPlayerSelected ] = useState<boolean>(false)
     const [ clicked, setClicked ] = useState<boolean>(false)
+    const [ playerWin, setPlayerWin ] = useState<boolean>(false)
+    const [ computerWin, setComputerWin ] = useState<boolean>(false)
+    const [ tieWin, setTieWin ] = useState<boolean>(false)  
         
-    
+    useEffect(() => {
+        if (result === 'You win') {
+          setPlayerWin(true);
+          setComputerWin(false);
+          setTieWin(false)
+        } 
+        if(result === 'You lose') {
+          setPlayerWin(false);
+          setComputerWin(true);
+          setTieWin(false)
+        }
+        if(result === "It's a tie"){
+            setPlayerWin(false);
+            setComputerWin(false);
+            setTieWin(true)
+        }
+
+
+    }, [result]);
 
     return (
-        <div className="flex gap-2 justify-center">
+        <div className="flex gap-2 justify-center ">
             <section className='flex flex-col items-center  relative left-[1rem]'>
                 <div className={`flex flex-col items-center mt-2 relative ] ${ playerSelected ? 'hidden':'flex'}`}  onClick={()=>(console.log('A card is selected'),  setPlayerSelected(!playerSelected))}>
                     <div className='flex gap-[5rem]'>
@@ -46,18 +67,29 @@ const Gameboard: React.FC<GameboardProps> = ({setScore}) => {
                         <img onClick={() => play('rock')} src={Rock} alt="paper" className='w-[11.375rem] h-[11.6875rem] cursor-pointer transition-transform transform hover:-translate-y-2'/>
                     </div>
                 </div> 
-                <section className={` w-full ${ playerSelected ? 'block':'hidden'} flex gap-8 items-center  `}>
+                <section className={` w-full ${ playerSelected ? 'block':'hidden'} flex gap-8 items-center  overflow-hidden`}>
                     <div className="flex flex-col gap-y-[2.9375rem] items-center">
                         <p className="text-shadow-md text-white text-[1.5rem] font-barlow font-bold tracking-wide">YOU PICKED</p>
-                        { playerChoice  && <img src={cards[playerChoice].image} alt={cards[playerChoice].name} className="w-[16.3125rem] h-[16.75rem]"/>}
+                        <div className="flex flex-col items-center justify-center ">
+                            { playerChoice  && <div  className={`left-[2.5rem] w-[11.3125rem] h-[11.75rem] absolute inset-0 top-[6.5rem]  ${ playerWin ? 'bg-white rounded-full opacity-75 animate-ping':''}`}></div>}
+                            { playerChoice  && <div  className={`left-[1.8rem] w-[12.5rem] h-[13rem] absolute inset-0 top-[6rem]  ${ playerWin ? 'bg-white rounded-full opacity-75 animate-ping':''}`}></div>}
+                            { playerChoice  && <div  className={`w-[14.3125rem] h-[14.75rem] left-[1rem] absolute inset-0 top-[5rem]  ${ playerWin ? 'bg-white rounded-full opacity-75 animate-ping':''}`}></div>}
+                            { playerChoice  && <img src={cards[playerChoice].image} alt={cards[playerChoice].name} className="w-[16.3125rem] h-[16.75rem] z-[9]"/>}
+                        </div> 
                     </div>
                     <div className="flex flex-col">
-                        <p className="text-shadow-md text-white text-[3.5rem] font-barlow font-bold tracking-wide text-center">{result}</p>
-                        <button className="hover:text-[#DB2E4D] bg-gradient-to-r from-gray-300 to-white h-[3rem] text-[1rem] font-semibold text-[#3b4262] tracking-wide rounded-[0.5rem] w-[13.75rem]" onClick={() => (reset() , setPlayerSelected(!playerSelected))}>Play Again</button>
+                        <p className={`text-shadow-md text-white text-[3.5rem] font-barlow font-bold tracking-wide text-center `}>{result}</p>
+                        <div className={` absolute inset-0 left-[21rem] top-[12.8rem] hover:text-[#DB2E4D] bg-gradient-to-r from-gray-300 to-white h-[1.75rem] text-[1rem] font-semibold text-[#3b4262] tracking-wide rounded-[0.5rem] w-[8.75rem] ${ tieWin ? 'animate-ping':''}`} ></div>
+                        <button className="hover:text-[#DB2E4D] bg-gradient-to-r from-gray-300 to-white h-[3rem] text-[1rem] font-semibold text-[#3b4262] tracking-wide rounded-[0.5rem] w-[13.75rem] z-[9]" onClick={() => (reset() , setPlayerSelected(!playerSelected))}>Play Again</button>
                     </div>
                     <div className="flex flex-col gap-y-[2.9375rem] items-center">
                         <p className="text-shadow-md text-white text-[1.5rem] font-barlow font-bold tracking-wide">THE HOUSE PICKED</p>
-                        {computerChoice && <img src={cards[computerChoice].image} alt={cards[computerChoice].name} className="w-[16.3125rem] h-[16.75rem]" />}
+                        <div className="flex flex-col items-center justify-center ">
+                            { computerChoice  && <div  className={`left-[37rem] w-[11.3125rem] h-[11.75rem] absolute inset-0 top-[6.5rem]  ${computerWin ? 'bg-white rounded-full opacity-75 animate-ping':''}`}></div>}
+                            { computerChoice  && <div  className={`left-[36.5rem] w-[12.5rem] h-[13rem] absolute inset-0 top-[6rem]  ${ computerWin ? 'bg-white rounded-full opacity-75 animate-ping':''}`}></div>}
+                            { computerChoice  && <div  className={`w-[14.3125rem] h-[14.75rem] left-[35.5rem] absolute inset-0 top-[5rem]  ${ computerWin ? 'bg-white rounded-full opacity-75 animate-ping':''}`}></div>}
+                            { computerChoice  && <img src={cards[computerChoice].image} alt={cards[computerChoice].name} className="w-[16.3125rem] h-[16.75rem] z-[9]"/>}
+                        </div>
                     </div>
                 </section>
             </section>
@@ -68,7 +100,7 @@ const Gameboard: React.FC<GameboardProps> = ({setScore}) => {
                     <h3 className="text-[1rem] font-semibold text-white uppercase">rules</h3>
                 </div>
             </section>
-            <section className={` flex justify-center items-center absolute bg-[#000]  w-full min-h-screen bg-opacity-50 inset-0 ${ clicked ? 'block' : 'hidden' }`}>
+            <section className={` flex justify-center items-center absolute bg-[#000] z-[999] w-full min-h-screen bg-opacity-50 inset-0 ${ clicked ? 'block' : 'hidden' }`}>
                 <div className="px-[2.7rem] py-[2rem] flex flex-col justify-center gap-y-[2.5rem] bg-white rounded-[0.5rem] w-[25rem] h-[25.9375rem]">
                     <div className="flex justify-between">
                         <h2 className="font-bold leading-[2rem] text-[#3b4262] text-[2rem]">RULES</h2>
